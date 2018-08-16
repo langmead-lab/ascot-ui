a4724667dabd1c690917bdf9bfc1e8e32625bbe4
Rone Charles <rone_charles@fastmail.com>
2018-08-16 13:39:26 -0400

    Fix search bar

--- server.R
+++ server.R
@@ -30,14 +30,22 @@ shinyServer(function(input, output, session) {
       "#D55E00",
       "#CC79A7"
     )
-  
+
   loaded_collections <- list()
-  
+
   collection <- reactive({
     if (is.null(loaded_collections[[input$dataset]])) {
       filename <- paste(input$dataset, "-dt", ".Rdata", sep = "")
       filename <- paste("data", filename, sep = "/")
       load(filename)
+      query_table <-
+        rename(
+          query_table,
+          visual = `Search Bar Lookup Value`,
+          value = `Gene Coverage Lookup Value`,
+          label = `Search Bar Visual`,
+          psi_value = `PSI Table Lookup Value`
+        )
       loaded_collections[[input$dataset]] <- list(
         psi = psi,
         gene_coverage = gene_coverage,
@@ -47,7 +55,7 @@ shinyServer(function(input, output, session) {
     }
     loaded_collections[[input$dataset]]
   })
-  
+
   observe({
     cell_groups <- unique(collection()$categories$cell_group)
     updateCheckboxGroupInput(session,
@@ -55,125 +63,124 @@ shinyServer(function(input, output, session) {
                              choices = cell_groups,
                              selected = cell_groups)
   })
-  
+
   observe({
     updateSelectizeInput(
       session,
       "gene",
-      choices = setNames(collection()$query_table[["Search Bar Lookup Value"]],
-                         collection()$query_table[["Search Bar Visual"]]),
-      selected = collection()$query_table[["Search Bar Lookup Value"]][1],
-      options = list(
-        render = I('{
-          item: function(item, escape) {
-            return "<div>" + escape(item.value) + "</div>";
-          }
-        }')
-      ), server = TRUE
+      choices = collection()$query_table,
+      selected = collection()$query_table[1][, value],
+      server = TRUE
     )
   })
-  
-  plot_data <- eventReactive(c(input$gene, input$cell_groups, input$colorblind_mode), {
-    if (input$gene == "") {
-      return()
-    }
-    
-    print(input$gene)
-    coverage <-
-      collection()$gene_coverage[gene_symbol == input$gene,
-                                 -(gene_id:gene_description)] %>%
-      melt(
-        measure.vars = patterns(".*"),
-        variable.name = "cell_type",
-        value.name = "gene_expression",
-        variable.factor = FALSE
-      )
-    coverage <-
-      coverage[collection()$categories,
-               on = "cell_type", nomatch = 0][cell_group %in% input$cell_groups]
-    
-    p1 <-
-      ggplot(coverage, aes(x = cell_type, y = gene_expression)) +
-      geom_bar(stat = "identity",
-               mapping = aes(fill = cell_group)) +
-      theme_minimal() +
-      scale_x_discrete("Cell Type",
-                       expand = c(0, 0),
-                       limits = coverage$cell_type) +
-      scale_y_continuous("Gene Expression (RPKM)", expand = c(0, 0)) +
-      theme(axis.text.x = element_text(
-        angle = 90,
-        hjust = 1,
-        vjust = 0.5
-      ))
-    
-    if (input$colorblind_mode) {
-      p1 <- p1 + scale_fill_manual("Cell Groups", values = cbPalette)
-    } else {
-      p1 <- p1 + scale_fill_brewer("Cell Groups", palette = "Set2")
-    }
-    
-    gene_psi <- collection()$psi[gene_symbol == input$gene,-(gene_symbol:MUTEX)] %>%
-      melt(
-        id.vars = c("exon_id"),
-        variable.name = "cell_type",
-        value.name = "PSI"
-      )
-    gene_psi <-
-      gene_psi[collection()$categories, on = "cell_type", nomatch = 0][cell_group %in% input$cell_groups]
-    
-    if (nrow(gene_psi) == 0) {
-      p2 <- NULL
-    } else {
-      group_labels <- unique(gene_psi$cell_group)
-      padding_width <-
-        sort(nchar(group_labels), decreasing = TRUE)[1]
-      scale_labels <-
-        stringr::str_pad(seq(0, 100, by = 25), width = padding_width, side = "right")
-      p2 <- ggplot(gene_psi, aes(x = cell_type, y = exon_id)) +
-        geom_tile(aes(fill = PSI), color = "white") +
-        theme_minimal() +
-        xlab("Cell Types") +
-        ylab("Exon ID") +
-        scale_x_discrete(expand = c(0, 0),
-                         limits = unique(gene_psi$cell_type)) +
-        theme(axis.text.x = element_text(
-          angle = 90,
-          hjust = 1,
-          vjust = 0.5
-        ))
-      if (input$colorblind_mode) {
-        p2 <-
-          p2 + scale_fill_distiller(
-            palette = "PuBuGn",
-            limits = c(0, 100),
-            labels = scale_labels
+
+  plot_data <-
+    eventReactive(
+      c(input$gene, input$cell_groups, input$colorblind_mode),
+      {
+        if (input$gene == "") {
+          return()
+        }
+
+        coverage <-
+          collection()$gene_coverage[gene_id == input$gene,-(gene_id:gene_description)] %>%
+          melt(
+            measure.vars = patterns(".*"),
+            variable.name = "cell_type",
+            value.name = "gene_expression",
+            variable.factor = FALSE
           )
-      } else {
-        p2 <-
-          p2 + scale_fill_distiller(
-            palette = "Spectral",
-            limits = c(0, 100),
-            labels = scale_labels
+        coverage <-
+          coverage[collection()$categories,
+                   on = "cell_type", nomatch = 0][cell_group %in% input$cell_groups]
+
+        p1 <-
+          ggplot(coverage, aes(x = cell_type, y = gene_expression)) +
+          geom_bar(stat = "identity",
+                   mapping = aes(fill = cell_group)) +
+          theme_minimal() +
+          scale_x_discrete("Cell Type",
+                           expand = c(0, 0),
+                           limits = coverage$cell_type) +
+          scale_y_continuous("Gene Expression (RPKM)", expand = c(0, 0)) +
+          theme(axis.text.x = element_text(
+            angle = 90,
+            hjust = 1,
+            vjust = 0.5
+          ))
+
+        if (input$colorblind_mode) {
+          p1 <- p1 + scale_fill_manual("Cell Groups", values = cbPalette)
+        } else {
+          p1 <- p1 + scale_fill_brewer("Cell Groups", palette = "Set2")
+        }
+
+        symbol <-
+          collection()$query_table[value == input$gene][1, psi_value]
+        gene_psi <-
+          collection()$psi[gene_symbol == symbol, -(gene_symbol:MUTEX)] %>%
+          melt(
+            id.vars = c("exon_id"),
+            variable.name = "cell_type",
+            value.name = "PSI"
           )
-      }
-    }
-    
-    if (!is.null(p2)) {
-      p1 <-
-        p1 + scale_x_discrete(NULL,
-                              expand = c(0, 0),
-                              limits = unique(gene_psi$cell_type)) +
-        theme(axis.text.x = element_blank())
-    }
-    list(p1 = p1,
-         p2 = p2,
-         size = if (is.null(p2))
-           500
-         else
-           800)
-  }, ignoreInit = TRUE, ignoreNULL = TRUE)
-  
+        gene_psi <-
+          gene_psi[collection()$categories, on = "cell_type", nomatch = 0][cell_group %in% input$cell_groups]
+        if (nrow(gene_psi) == 0) {
+          p2 <- NULL
+        } else {
+          group_labels <- unique(gene_psi$cell_group)
+          padding_width <-
+            sort(nchar(group_labels), decreasing = TRUE)[1]
+          scale_labels <-
+            stringr::str_pad(seq(0, 100, by = 25), width = padding_width, side = "right")
+          p2 <- ggplot(gene_psi, aes(x = cell_type, y = exon_id)) +
+            geom_tile(aes(fill = PSI), color = "white") +
+            theme_minimal() +
+            xlab("Cell Types") +
+            ylab("Exon ID") +
+            scale_x_discrete(expand = c(0, 0),
+                             limits = unique(gene_psi$cell_type)) +
+            theme(axis.text.x = element_text(
+              angle = 90,
+              hjust = 1,
+              vjust = 0.5
+            ))
+          if (input$colorblind_mode) {
+            p2 <-
+              p2 + scale_fill_distiller(
+                palette = "PuBuGn",
+                limits = c(0, 100),
+                labels = scale_labels
+              )
+          } else {
+            p2 <-
+              p2 + scale_fill_distiller(
+                palette = "Spectral",
+                limits = c(0, 100),
+                labels = scale_labels
+              )
+          }
+        }
+
+        if (!is.null(p2)) {
+          p1 <-
+            p1 + scale_x_discrete(NULL,
+                                  expand = c(0, 0),
+                                  limits = unique(gene_psi$cell_type)) +
+            theme(axis.text.x = element_blank())
+        }
+        list(p1 = p1,
+             p2 = p2,
+             size = if (is.null(p2))
+               500
+             else
+               800)
+      },
+      ignoreInit = TRUE,
+      ignoreNULL = TRUE
+    )
+
   observeEvent(plot_data(), {
     output$plot <- renderPlot({
       if (is.null(plot_data()$p2)) {
--- ui.R
+++ ui.R
@@ -15,7 +15,6 @@ library(magrittr)
 
 shinyUI(
   fluidPage(
-    # fluidRow(headerPanel("Header")),
     theme = shinythemes::shinytheme("spacelab"),
     tags$head(tags$style(
       HTML(
@@ -28,72 +27,78 @@ shinyUI(
         -moz-column-fill: auto;
         -column-fill: auto;
         }"
-)
-      )),
+      )
+    )),
 
-wellPanel(fluidRow(
-  column(
-    4,
-    selectInput(
-      "dataset",
-      tags$b("Select Dataset"),
-      choices = c("Super Mouse" = "supermouse", "Allen Brain" = "allenbrain"),
-      selected = "supermouse"
-    )
-  ),
-  column(
-    8,
-    selectizeInput(
-      "gene",
-      tags$b("Select Gene"),
-      choices = NULL,
-      options = list(
-        maxItems = "1",
-        # plugins = list('restore_on_backspace')
-                       onFocus = I('function () {
-                                          var value = this.getValue();
-                                          console.log(value);
-                                          if (value.length > 0) {
-                                            this.clear(true);
-                                            this.setTextboxValue(value);
-                                          }
-                                   }'),
-                      onItemAdd = I('function() { this.blur(); }')
+    wellPanel(fluidRow(
+      column(
+        4,
+        selectInput(
+          "dataset",
+          tags$b("Select Dataset"),
+          choices = c("Super Mouse" = "supermouse", "Allen Brain" = "allenbrain"),
+          selected = "supermouse"
+        )
+      ),
+      column(8,
+             selectizeInput("gene",
+                            tags$b("Select Gene"),
+                            choices = NULL,
+                            options = list(
+                              # plugins = list('restore_on_backspace'),
+                              searchField = c("label"),
+                              onFocus = I('
+                                          function () {
+                                            var value = this.getLabel();
+                                            console.log(value);
+                                            if (value.length > 0) {
+                                              this.clear(true);
+                                              this.setTextboxValue(value);
+                                            }
+                                          }'),
+                              onItemAdd = I('function() { this.blur(); }'),
+                              render = I('{
+                                            item: function(item, escape) {
+                                              return "<div>" + escape(item.visual) + "</div>";
+                                            },
+                                            option: function(item, escape) {
+                                              var res = item.label.split(/(\\s+)/);
+                                              return "<div>" + escape(res[0]) + "&nbsp;<i>" + escape(res[2]) + "</i></div>";
+                                            }
+                                          }')
+      )))
+    )),
+    plotOutput("plot", height = "500px") %>% withSpinner(),
+    # plotOutput("heatmap", height = "500px"),
+    absolutePanel(
+      id = "controls",
+      fixed = TRUE,
+      draggable = FALSE,
+      top = "auto",
+      left = 10,
+      right = "auto",
+      bottom = 0,
+      width = 330,
+      height = "auto",
+      bsCollapsePanel(
+        "Plot Options",
+        style = "primary",
+        wellPanel(checkboxInput(
+          "colorblind_mode", "Enable Colorblind Mode"
+        )),
+        wellPanel(tags$div(
+          align = "left",
+          class = "muticol",
+          checkboxGroupInput(
+            "cell_groups",
+            "Filter Cell Groups",
+            choices = NULL,
+            selected = NULL
+          )
+        )),
+        wellPanel(uiOutput("exon_filter_widget")),
+        wellPanel(uiOutput("save_widget"))
       )
     )
   )
-)),
-plotOutput("plot", height = "500px") %>% withSpinner(),
-# plotOutput("heatmap", height = "500px"),
-absolutePanel(
-  id = "controls",
-  fixed = TRUE,
-  draggable = FALSE,
-  top = "auto",
-  left = 10,
-  right = "auto",
-  bottom = 0,
-  width = 330,
-  height = "auto",
-  bsCollapsePanel(
-    "Plot Options",
-    style = "primary",
-    wellPanel(checkboxInput(
-      "colorblind_mode", "Enable Colorblind Mode"
-    )),
-    wellPanel(tags$div(
-      align = "left",
-      class = "muticol",
-      checkboxGroupInput(
-        "cell_groups",
-        "Filter Cell Groups",
-        choices = NULL,
-        selected = NULL
-      )
-    )),
-    wellPanel(uiOutput("exon_filter_widget")),
-    wellPanel(uiOutput("save_widget"))
-  )
-)
-)
 )
